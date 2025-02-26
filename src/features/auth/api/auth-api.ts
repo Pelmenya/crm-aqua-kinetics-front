dbfb6b6781de7da2f7c7a615cf08cea1d164ff80
Dmitry Lyapin <lyapindm@yandex.ru>
2025-02-26 15:54:48 +0300

    Add to .env BASE backend url

--- src/features/auth/api/auth-api.ts
+++ src/features/auth/api/auth-api.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://newly-wired-grouse.cloudpub.ru',
+        baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
     }),
 
     endpoints: (builder) => ({
@@ -18,14 +18,7 @@ export const authApi = createApi({
                 credentials: 'include'
             }),
         }),
-        getAuth: builder.query<string | undefined, string | undefined>({
-            query: () => ({
-                url: '',
-                method: 'GET',
-            }),
-        }),
-
     }),
 });
 
-export const { usePostAuthMutation, useGetAuthQuery } = authApi;
+export const { usePostAuthMutation } = authApi;
