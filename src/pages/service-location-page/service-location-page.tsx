ffd1c342cce54ca1241555dcf3b3c6b5c90cb0aa
Dmitry Lyapin <lyapindm@yandex.ru>
2025-03-10 12:09:22 +0300

    Add save location of Service man

--- src/pages/service-location-page/service-location-page.tsx
+++ src/pages/service-location-page/service-location-page.tsx
@@ -1,73 +1,91 @@
 import { FC, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { FormWithTitle } from "@/shared/ui/components/form-with-title/form-with-title";
 import { AddressSearchWithMap } from "@/features/address-search/ui/address-search-with-map";
 import { getAccountServiceState } from "@/entities/account-service/model/account-service-selectors";
-import { useAppDispatch } from "@/shared/lib/hooks/use-app-dispatch";
-import { useAppSelector } from "@/shared/lib/hooks/use-app-selector";
 import { setAddress, setCoordinates, setRadiusKm } from "@/entities/account-service/model/account-service-slice";
 import { Page } from "@/shared/ui/components/page/page";
-import { TCreateAccountService, useCreateOrUpdateAccountServiceMutation } from "@/entities/account-service/api/account-service-api";
+import { TCreateAccountService, useCreateOrUpdateAccountServiceMutation, useGetAccountServiceByUserQuery } from "@/entities/account-service/api/account-service-api";
 import { useLaunchParams } from "@telegram-apps/sdk-react";
 import { RangeSlider } from "@/shared/ui/components/range-slider.tsx/range-slider";
 import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "@/shared/lib/hooks/use-app-dispatch";
+import { useAppSelector } from "@/shared/lib/hooks/use-app-selector";
+import { Loading } from "@/shared/ui/components/loading/loading";
 
 export type TLocationFormInputs = {
     radiusKm: number;
 };
 
-const schema = yup.object().shape({
-    radiusKm: yup
-        .number()
-        .nullable()
-        .typeError("Значение должно быть числом")
-        .required("Радиус не должен быть пустым")
-        .min(0, "Радиус не может быть отрицательным"),
-});
-
 export const ServiceLocationPage: FC = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
-    const accountService = useAppSelector(getAccountServiceState);
+    const accountServiceFromRedux = useAppSelector(getAccountServiceState);
     const [createAccountService, { isLoading }] = useCreateOrUpdateAccountServiceMutation();
 
-    const { handleSubmit, setValue } = useForm<TLocationFormInputs>({
-        resolver: yupResolver(schema),
-    });
+    const { handleSubmit, setValue } = useForm<TLocationFormInputs>();
+
     const lp = useLaunchParams();
     const authKey = lp.initDataRaw || '';
+
+    const { data: accountServiceFromBack, refetch, isFetching } = useGetAccountServiceByUserQuery(authKey, {
+        refetchOnMountOrArgChange: true,
+    });
+
+    useEffect(() => {
+        if (authKey) {
+            refetch();
+        }
+    }, [authKey, refetch]);
+
+    useEffect(() => {
+        if (accountServiceFromBack) {
+            // Обновляем только если данные изменились
+            if (accountServiceFromBack.address !== accountServiceFromRedux.address) {
+                dispatch(setAddress(accountServiceFromBack.address || null));
+            }
+            if (
+                accountServiceFromBack.coordinates &&
+                (accountServiceFromBack.coordinates.latitude !== accountServiceFromRedux.coordinates?.latitude ||
+                    accountServiceFromBack.coordinates.longitude !== accountServiceFromRedux.coordinates?.longitude)
+            ) {
+                dispatch(setCoordinates(accountServiceFromBack.coordinates));
+            }
+            if (accountServiceFromBack.radiusKm !== accountServiceFromRedux.radiusKm) {
+                dispatch(setRadiusKm(accountServiceFromBack.radiusKm));
+                setValue('radiusKm', Number(accountServiceFromBack.radiusKm));
+            }
+        }
+    }, [accountServiceFromBack, dispatch, setValue]);
 
     const onSubmit = async () => {
         try {
             const newAccountService: TCreateAccountService = {
-                ...accountService,
-                coordinates: accountService.coordinates ? {
+                ...accountServiceFromRedux,
+                coordinates: accountServiceFromRedux.coordinates ? {
                     type: 'Point',
-                    coordinates: [accountService.coordinates.longitude, accountService.coordinates.latitude]
+                    coordinates: [accountServiceFromRedux.coordinates.longitude, accountServiceFromRedux.coordinates.latitude]
                 } : null
             };
 
-            await createAccountService({ newAccountService, authKey }).unwrap();
+           const account = await createAccountService({ newAccountService, authKey }).unwrap();
+           console.log(account);
             navigate('/account');
 
         } catch (error) {
-            console.error("Failed to create account service", error);
+            console.log("Failed to create account service", error);
         }
     };
 
-    useEffect(() => {
-        if (accountService.radiusKm !== undefined && accountService.radiusKm !== null) {
-            setValue('radiusKm', accountService.radiusKm, { shouldValidate: true });
-        }
-    }, [accountService.radiusKm, setValue]);
-
-    // Обработка изменения значения радиуса
     const handleRadiusChange = (value: number) => {
         setValue('radiusKm', value);
         dispatch(setRadiusKm(value));
     };
+
+    // Проверка состояния загрузки
+    if (isFetching) {
+        return <Loading color="text-primary" size="loading-xs" type="loading-infinity" />;
+    }
 
     return (
         <Page back={true}>
@@ -77,14 +95,14 @@ export const ServiceLocationPage: FC = () => {
                     onSubmit={handleSubmit(onSubmit)}
                     submitButtonText="Сохранить"
                     isLoading={isLoading}
-                    isDisabledSubmitBtn={!accountService.address || !accountService.radiusKm}
+                    isDisabledSubmitBtn={!accountServiceFromRedux.address || !accountServiceFromRedux.radiusKm}
                 >
                     <AddressSearchWithMap
-                        query={accountService.address || ''}
-                        selectedAddress={accountService.address}
-                        coordinates={accountService.coordinates || null}
+                        query={accountServiceFromRedux.address || ''}
+                        selectedAddress={accountServiceFromRedux.address}
+                        coordinates={accountServiceFromRedux.coordinates || null}
                         isViewCoordinates={false}
-                        radiusKm={accountService.radiusKm || 0}
+                        radiusKm={accountServiceFromRedux.radiusKm || 0}
                         onQueryChange={(query) => dispatch(setAddress(query))}
                         onSelectAddress={(address) => dispatch(setAddress(address))}
                         onCoordinatesChange={(coords) => dispatch(setCoordinates(coords))}
@@ -92,16 +110,16 @@ export const ServiceLocationPage: FC = () => {
                     />
                     <div className="grid w-full">
                         <RangeSlider
-                            value={accountService.radiusKm || 0}
+                            value={accountServiceFromRedux.radiusKm || 0}
                             onChange={handleRadiusChange}
                             min={0}
-                            max={150} // Укажите максимальное значение радиуса
+                            max={150}
                             label="Радиус выезда"
                             unit="км."
                         />
                     </div>
                 </FormWithTitle>
             </div>
-        </Page >
+        </Page>
     );
 };
