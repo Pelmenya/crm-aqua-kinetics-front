8531ef89ee038a9d604e4512da68231b1bfa8a6f
Dmitry Lyapin <lyapindm@yandex.ru>
2025-03-20 17:05:58 +0300

    Top-level-group-card layout

--- src/features/moy-sklad/ui/top-level-group-card/top-level-group-card.tsx
+++ src/features/moy-sklad/ui/top-level-group-card/top-level-group-card.tsx
@@ -2,12 +2,11 @@ import { Base } from "@/shared/ui/components/base/base";
 import { FC } from "react";
 import { TGroup } from "../../api/moy-sklad-api";
 import imgSoftener from './imgSoftener.png';
+import imgDwm from './imgDwm.png';
+import imgLinefilters from './imgLinefilters.png';
 
 export const TopLevelGroupCard: FC<{ group: TGroup }> =
     ({ group }) => {
-
-        
-
         return (
             <Base className="px-4 pt-0 pb-0 w-full relative">
                 <div className="min-h-[115px] max-h-[115px] flex items-center w-full">
@@ -16,7 +15,7 @@ export const TopLevelGroupCard: FC<{ group: TGroup }> =
                         <p className="text-xs tracking-tight opacity-60">{group?.bundle?.description}</p>
                         <h5 className="text-md font-bold tracking-tight text-primary">{"от 49 900Р"}</h5>
                     </div>
-                    <img className="max-h-[100px] absolute bottom-0 right-[12px]" src={imgSoftener} alt={group.groupName || ''}/>
+                    <img className="max-h-[100px] absolute bottom-0 right-4" src={group.groupName === 'Waterboss' ? imgSoftener: imgLinefilters} alt={group.groupName || ''}/>
                 </div>
             </Base>)
 
