86720ede4ce437b49ddb3f12da37fc815fd154ba
Dmitry Lyapin <lyapindm@yandex.ru>
2025-03-27 10:28:48 +0300

    Add component CollapsibleDescription

--- src/pages/product-page/product-page.tsx
+++ src/pages/product-page/product-page.tsx
@@ -1,27 +1,27 @@
+import { FC } from 'react';
 import { useGetProductQuery } from "@/features/moy-sklad/api/moy-sklad-api";
 import { ProductSlider } from "@/features/moy-sklad/ui/product-slider/product-slider";
 import { Page } from "@/shared/ui/components/page/page";
-import { FC } from "react";
 import { useParams } from "react-router-dom";
+import { CollapsibleDescription } from '@/shared/ui/components/collapsible-description/collapsible-description';
 
 export const ProductPage: FC = () => {
     const { id } = useParams<{ id: string }>();
     const { data: product, error: errorProduct, isLoading: isLoadingProduct } = useGetProductQuery(id || '');
 
     if (isLoadingProduct) {
-        return <div>Грузим</div>
+        return <div>Грузим</div>;
     }
 
-    if (errorProduct) return <div>Сломалось</div>
+    if (errorProduct) return <div>Сломалось</div>;
 
-    console.log()
     return (
         <Page className="pb-16">
             {product && <ProductSlider id={product.id} />}
-            <div className="bg-base-100 rounded-t-lg p-4 mt-[-2rem]">
-                <h1 className="text-xl w-[60%]">{product?.name}</h1>
+            <div className="bg-base-100 rounded-t-lg p-4 mt-[-2rem] flex flex-col gap-2">
+                <h1 className="text-xl opacity-80 w-full">{product?.name}</h1>
+                {product?.description && <CollapsibleDescription description={product.description} />}
             </div>
-            
         </Page>
-    )
-}
+    );
+};
---
+++ src/shared/ui/components/collapsible-description/collapsible-description.tsx
@@ -0,0 +1,59 @@
+import cn from 'classnames';
+import { FC, useEffect, useRef, useState } from 'react';
+
+export const CollapsibleDescription: FC<{ description: string }> = ({ description }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [isButtonVisible, setIsButtonVisible] = useState(true);
+    const descriptionRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (descriptionRef.current) {
+            const lineHeight = parseInt(window.getComputedStyle(descriptionRef.current).lineHeight);
+            const maxHeight = lineHeight * 2; // Высота для двух строк
+            if (descriptionRef.current.scrollHeight <= maxHeight) {
+                setIsButtonVisible(false); // Если текста меньше или равно двум строкам, кнопка скрыта
+            }
+        }
+    }, []);
+
+    const toggleDescription = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    return (
+        <div className="max-w-md mx-auto">
+            <div className="relative overflow-hidden" style={{ transition: 'max-height 0.3s ease-in-out' }} ref={descriptionRef}>
+                <div
+                    className={cn('transition-opacity duration-300 ease-in-out', {
+                        'max-h-full opacity-100': isExpanded,
+                        'max-h-[4rem] opacity-75': !isExpanded
+                    })}
+                >
+                    <p className="line-clamp-none text-sm">{description}</p>
+                </div>
+                {!isExpanded && isButtonVisible && (
+                    <div className="absolute bottom-0 left-0 right-0 h-14 bg-gradient-to-t from-base-100 to-transparent transition-opacity duration-300 ease-in-out"></div>
+                )}
+            </div>
+            {isButtonVisible && (
+                <button
+                    className={cn('btn btn-link btn-xs flex items-center w-full transition-all duration-300 ease-in-out', {
+                        'mt-0': isExpanded,
+                        'mt-[-0.5rem]': !isExpanded
+                    })}
+                    onClick={toggleDescription}
+                >
+                    <span className={cn('transform transition-transform duration-300 ease-in-out', {
+                        'rotate-180': isExpanded,
+                        'rotate-0': !isExpanded
+                    })}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-4">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </span>
+                </button>
+            )}
+        </div>
+    );
+};
+
